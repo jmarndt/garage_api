1642e19457eadc0a8022afb216adc0910f683f9e
Joe Arndt <jmarndt@users.noreply.github.com>
2023-10-23 12:43:39 -0500

    fixed run file

--- install/run.py
+++ install/run.py
@@ -1,5 +1,10 @@
 import uvicorn
 
 
+API = 'app.main:app'
+HOST = '0.0.0.0'
+PORT = 8001
+
+
 if __name__ == '__main__':
-    uvicorn.run(API, host='0.0.0.0', port=8001)
+    uvicorn.run(API, host=HOST, port=PORT)
