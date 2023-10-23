65bcbf6ca14aa3fbdf7db0dd2c9b3217b75f3415
Joe Arndt <jmarndt@users.noreply.github.com>
2023-10-23 10:27:06 -0500

    refined running/install process

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Garage API
 This API control lights and doors for the garage. It runs on a Raspbeery Pi (zero W in my case), connected to an 8-relay module and a modified Lutron Pico remote.
 
-## Dependencies
+## Development
 Create a virtual environment with
 ```
 python3 -m venv venv && source venv/bin/activate
@@ -11,11 +11,11 @@ Install depndencies with
 ```
 python3 -m pip install --upgrade pip && python3 -m pip install -r requirements.txt
 ```
+### Run Locally
+Run locally with `./run.sh`
 
 ## Authentication
 The API is secured with a key. On startup it is expecting to find an environment variable called `API_KEY`. This needs to be set before run, or stored in a `.env` file (see `example.env`).
 
-## Running
-For local development and testing, run `python3 run.py`.
-In prodcution add the `--prod` flag: `python3 run.py --prod`.
-For best results, create a system service to run this on startup. This can be done with `create_systemd_service.py`.
+## Installing
+Installing will copy the `app` folder to `/opt/garage_api` install dependendies and create a systemd service. To install, `cd` into `install` and run `./install.sh`
---
+++ install/install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+INSTALL_DIR=/opt/garage_api
+APP_DIR=../app
+RUN_FILE=run_garage_api.sh
+SERVICE_NAME=garage_api.service
+SERVICE_FILE_CONTENTS=garage_api_service
+SERVICE_FILE=/etc/systemd/system/$SERVICE_NAME
+
+cd $(dirname -- "$0")
+
+write_log() {
+    echo "[$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)] INSTALL: $1" >> $LOG_FILE
+}
+
+update_system() {
+    write_log "Updating system...."
+    apt update
+    apt full-upgrade -yq
+}
+
+install_deps() {
+    write_log "Installing dependencies...."
+    apt install -yq python3 python3-pip
+    python3 -m pip install -r ../requirements.txt
+}
+
+install_api_files() {
+    write_log "Installing API to $INSTALL_DIR...."
+    if [ -d $INSTALL_DIR ]; then
+        rm -rf $INSTALL_DIR
+    fi
+    mkdir $INSTALL_DIR
+    cp -r $APP_DIR $INSTALL_DIR
+    cp $RUN_FILE $INSTALL_DIR
+}
+
+create_service() {
+    if systemctl list-unit-files "$SERVICE_NAME"; then
+        systemctl stop $SERVICE_NAME
+    fi
+    write_log "Creating $SERVICE_FILE...."
+    cat $SERVICE_FILE_CONTENTS > $SERVICE_FILE
+}
+
+enable_service() {
+    write_log "Enabling $SERVICE_NAME...."
+    systemctl daemon-reload
+    systemctl enable $SERVICE_NAME
+    systemctl start $SERVICE_NAME
+}
+
+do_install() {
+    write_log "Beginning Garage API install...."
+    update_system
+    install_deps
+    install_api_files
+    create_service
+    enable_service
+    write_log "Garage API install complete!"
+    #reboot
+}
+
+do_install
