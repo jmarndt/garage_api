fa192fdb71548c5e9c4bbacb08c97fa5fb697bd5
Joe Arndt <jmarndt@users.noreply.github.com>
2024-05-16 21:14:11 -0500

    added query param auth option

--- app/authentication.py
+++ app/authentication.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import Depends, status, HTTPException
+from fastapi import Depends, status, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 
 
@@ -9,7 +9,9 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-def authenticate(api_key: str = Depends(oauth2_scheme)):
-    if api_key != API_KEY:
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
+def authenticate(req: Request, header_api_key: str = Depends(oauth2_scheme)):
+    query_api_key = req.query_params.get('token')
+    
+    if header_api_key != API_KEY and query_api_key != API_KEY:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
